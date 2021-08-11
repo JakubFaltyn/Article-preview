a038b80980c3cb6849978236153beeac30ce6ea0
JakubFaltyn <jakubfaltyn@gmail.com>
2021-08-12 00:50:36 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const share = document.querySelector(".share");
+const shareBtn = document.querySelector(".share-btn");
+
+shareBtn.addEventListener("click", function () {
+  share.classList.toggle("show");
+});
